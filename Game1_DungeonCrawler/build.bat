a4d2c66c2ee901f1a67d7ff17a75bbfd1fd6ab2f
Helex315 <helex315@gmail.com>
2025-11-25 07:57:48 -0600

    bat file update

--- Game1_DungeonCrawler/build.bat
+++ Game1_DungeonCrawler/build.bat
@@ -1,5 +1,5 @@
 ::@echo off
-javac Game.java View.java Controller.java Model.java Tree.java Link.java Json.java Sprite.java TreasureChests.java Boomerang.java
+javac Game.java View.java Controller.java Model.java Tree.java Link.java Json.java Sprite.java TreasureChests.java Boomerang.java RoomTemplate.java
 if %errorlevel% neq 0 (
 	echo There was an error; exiting now.	
 ) else (
